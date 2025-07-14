309ad4c093f2179597d20914338307b52c82da70
Simonfitz <fitzges5@tcd.ie>
2025-07-14 18:57:31 +0100

    Add ranger

--- tiniarmy/scenes/game/game.gd
+++ tiniarmy/scenes/game/game.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const UnitScene: PackedScene = preload("res://scenes/unit/unit.tscn")
 const UNIT_FIGHTER = preload("res://resources/units/unit_fighter.tres")
+const UNIT_RANGER = preload("res://resources/units/unit_ranger.tres")
+const UNITS = [UNIT_FIGHTER, UNIT_RANGER]
 
 @onready var red_spawn: SpawnPoint = $SpawnPoints/RedSpawn
 @onready var blue_spawn: SpawnPoint = $SpawnPoints/BlueSpawn
@@ -16,11 +18,11 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent):
 	#if (event is InputEventJoypadButton or event is InputEventJoypadMotion) and event.device + 1 == _idx:
 	if event.is_action_pressed("Left", true):
-		SpawnRed(UNIT_FIGHTER)
+		SpawnRed(UNITS.pick_random())
 		return
 	
 	if event.is_action_pressed("Right", true):
-		SpawnBlue(UNIT_FIGHTER)
+		SpawnBlue(UNITS.pick_random())
 		return
 
 func SpawnRed(unitInfo: UnitInfo):
